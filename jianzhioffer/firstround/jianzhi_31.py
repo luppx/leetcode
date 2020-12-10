69ade2c38cca655aed892536f92ce2100a91d60c
slu <strikerlu@foxmail.com>
2020-12-10 12:03:12 +0800

    add jianzhi_31.py

--- jianzhioffer/firstround/jianzhi_31.py
+++ jianzhioffer/firstround/jianzhi_31.py
@@ -5,25 +5,18 @@ class Solution(object):
         :type popped: List[int]
         :rtype: bool
         """
-        if not pushed and not popped:
-            return True
-        elif not pushed or not popped:
-            return False
-        query = [pushed.pop(0)]
-        while popped:
-            if popped[0] not in query:
-                query.append(pushed.pop(0))
-            elif query[-1] != popped[0]:
-                return False
-            else:
-                query.pop(-1)
-                popped.pop(0)
-        return True
+        stack, i = [], 0
+        for num in pushed:
+            stack.append(num)
+            while stack and stack[-1] == popped[i]:
+                stack.pop()
+                i += 1
+        return not stack
 
 
 if __name__ == '__main__':
     p = [1,2,3,4,5]
-    q = [[4,3,5,1,2]]
+    q = [4,5,3,2,1]
     obj = Solution()
     result = obj.validateStackSequences(p, q)
     print(result)
