267f7c572643c7acdfa544d31cc53703de567af7
slu <strikerlu@foxmail.com>
2020-12-13 20:54:32 +0800

    Add jianzhi_32_1.py, jianzhi_32_3.py

---
+++ jianzhioffer/firstround/jianzhi_32_1.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        queue = [root]
+        res = []
+        while queue:
+            node = queue.pop(0)
+            res.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return res
+
+
+if __name__ == '__main__':
+    n1 = TreeNode(3)
+    n2 = TreeNode(9)
+    n3 = TreeNode(20)
+    n4 = TreeNode(15)
+    n5 = TreeNode(7)
+
+    n1.left = n2
+    n1.right = n3
+    n3.left = n4
+    n3.right = n5
+
+    obj = Solution()
+    result = obj.levelOrder(n1)
+    print(result)
---
+++ jianzhioffer/firstround/jianzhi_32_3.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        queue = [root]
+        res = []
+        while queue:
+            tmp = []
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                tmp.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(tmp[::-1] if len(res) % 2 else tmp)
+        return res
+
+
+if __name__ == '__main__':
+    n1 = TreeNode(3)
+    n2 = TreeNode(9)
+    n3 = TreeNode(20)
+    n4 = TreeNode(15)
+    n5 = TreeNode(7)
+
+    n1.left = n2
+    n1.right = n3
+    n3.left = n4
+    n3.right = n5
+
+    obj = Solution()
+    result = obj.levelOrder(n1)
+    print(result)
