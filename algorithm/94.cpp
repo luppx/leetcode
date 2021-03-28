01b753cf0eb64979bd47eb9e54a906cf2f810a38
slu <strikerlu@foxmail.com>
2021-03-28 19:29:26 +0800

    add 94.cpp

---
+++ algorithm/94.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <stack>
+#include <cstddef>
+using namespace std;
+
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x): val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+	vector<int> inorderTraversal(TreeNode* root);
+};
+
+vector<int> Solution::inorderTraversal(TreeNode* root)
+{
+	if(root==NULL) return {};
+	
+	vector<int> res = {};
+	stack<TreeNode*> s;
+	
+	while(!s.empty() || root!=NULL)
+	{
+		while(root!=NULL)
+		{
+			s.push(root);
+			root = root->left;
+		}
+
+		root = s.top();
+		s.pop();
+		res.push_back(root->val);
+		root = root->right;
+	}
+	return res;
+}
+
+int main()
+{
+	return 0;
+}
+
