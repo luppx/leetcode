f8515bfb5531e598bd2a013640d1e3bb28461267
slu <strikerlu@foxmail.com>
2023-08-15 00:25:16 +0800

    feat: add 160.go

---
+++ algorithm/go/firstround/160.go
@@ -0,0 +1,24 @@
+package firstround
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+
+	pa, pb := headA, headB
+	for pa != pb {
+		if pa == nil {
+			pa = headB
+		} else {
+			pa = pa.Next
+		}
+
+		if pb == nil {
+			pb = headA
+		} else {
+			pb = pb.Next
+		}
+	}
+
+	return pa
+}
