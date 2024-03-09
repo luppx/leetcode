de4ae82e7b6530e1f4eb01c4af49270f4c3f55cc
slu <strikerlu@foxmail.com>
2024-03-09 12:09:13 +0800

    feat: add 538.go

---
+++ algorithm/go/firstround/538.go
@@ -0,0 +1,27 @@
+package firstround
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 反向中序遍历,先遍历右子树,再遍历左子树.时间复杂度O(n),空间复杂度O(n)
+func convertBST(root *TreeNode) *TreeNode {
+	if root == nil || (root.Left == nil && root.Right == nil) {
+		return root
+	}
+	traverse(root, 0)
+	return root
+}
+
+func traverse(node *TreeNode, greaterSum int) int {
+	if node == nil {
+		return greaterSum
+	}
+
+	sumRight := traverse(node.Right, greaterSum)
+	node.Val += sumRight
+	sumLeft := traverse(node.Left, node.Val)
+	return sumLeft
+}
