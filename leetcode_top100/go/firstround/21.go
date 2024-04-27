f11b41343383b48a7f0734e40476bd0c07b48cf0
slu <strikerlu@foxmail.com>
2024-04-27 15:51:48 +0800

    feat: add 21.go

---
+++ leetcode_top100/go/firstround/21.go
@@ -0,0 +1,37 @@
+package firstround
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+
+	dummy := &ListNode{}
+	cur, cur1, cur2 := dummy, list1, list2
+	for cur1 != nil && cur2 != nil {
+		if cur1.Val <= cur2.Val {
+			cur.Next = cur1
+			cur1 = cur1.Next
+		} else {
+			cur.Next = cur2
+			cur2 = cur2.Next
+		}
+		cur = cur.Next
+	}
+
+	if cur1 != nil {
+		cur.Next = cur1
+	}
+	if cur2 != nil {
+		cur.Next = cur2
+	}
+
+	return dummy.Next
+}
